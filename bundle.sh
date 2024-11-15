bb6630a5accc03557594adbca406a68d496b6d69
aromarious <6535448+aromarious@users.noreply.github.com>
2024-11-15 20:14:29 +0900

    [feat] package.json, バンドル用スクリプト, タスク設定ファイルを追加

---
+++ bundle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 引数から入力ファイルを取得
+input_file="$1"
+echo "Input file: $input_file"
+
+# 入力ファイル名を加工して出力ファイル名を作成
+output_file=$(basename "$input_file" .js)
+output_file="dist/${output_file/ToBundle/}.js"
+echo "Output file: $output_file"
+
+# esbuildコマンドを実行
+npx esbuild "$input_file" --bundle --platform=node --format=cjs --outfile="$output_file"
+
+echo "Bundled $input_file to $output_file"
