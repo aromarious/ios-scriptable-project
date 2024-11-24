cb8f5d04d4c35f4a7383d2da05f10326297f1a35
aromarious <6535448+aromarious@users.noreply.github.com>
2024-11-24 16:49:42 +0900

    [doc] コード補完のためのJSDoc追加

--- src/discord-rest2bundle.js
+++ src/discord-rest2bundle.js
@@ -1,16 +1,41 @@
+/**
+ * Discord REST API wrapper for Scriptable
+ * @module discord-rest
+ */
 const { Routes } = require("discord-api-types/v10")
 
-var REST4Scriptable = class _REST4Scriptable {
+/**
+ * REST4Scriptable class for interacting with the Discord REST API.
+ */
+class REST4Scriptable {
+  /**
+   * @param {Object} options - Configuration options for the REST4Scriptable instance.
+   * @param {number} options.version - Discord API version.
+   */
   constructor(options = {}) {
     this.version = options.version
     this.token = null
   }
 
+  /**
+   * Sets the token for the REST API.
+   * @param {string} token - Discord Bot token.
+   * @returns {this} - The instance of the REST4Scriptable class.
+   */
   setToken(token) {
     this.token = token
     return this
   }
 
+  /**
+   * Sends a request to the Discord REST API.
+   * @param {string} method - HTTP method (GET, POST, etc.).
+   * @param {string} route - Discord API route created with Routes method.
+   * @param {Object} options - Additional request options.
+   * @param {Object} [options.headers] - Additional headers to include in the request.
+   * @param {Object} [options.body] - The body of the request.
+   * @returns {Promise<*>} - The response from the API.
+   */
   async request(method, route, options = {}) {
     const url = `https://discord.com/api/v${this.version}${route}`
     const req = new Request(url)
@@ -32,25 +57,62 @@ var REST4Scriptable = class _REST4Scriptable {
     return res
   }
 
+  /**
+   * Sends a GET request to the Discord API.
+   * @param {string} route - Discord API route created with Routes method.
+   * @param {Object} options - Additional request options.
+   * @param {Object} [options.headers] - Additional headers to include in the request.
+   * @returns {Promise<*>} - The response from the API.
+   */
   get(route, options) {
     return this.request("GET", route, options)
   }
 
+  /**
+   * Sends a DELETE request to the specified route with the given options.
+   * @param {string} route - The API endpoint to send the DELETE request to.
+   * @param {Object} [options] - Optional settings for the request.
+   * @returns {Promise<*>} - The response from the API.
+   */
   delete(route, options) {
     return this.request("DELETE", route, options)
   }
 
+  /**
+   * Sends a POST request to the Discord API.
+   * @param {string} route - Discord API route created with Routes method.
+   * @param {Object} options - Additional request options.
+   * @param {Object} [options.headers] - Additional headers to include in the request.
+   * @param {Object} [options.body] - The body of the request.
+   * @returns {Promise<*>} - The response from the API.
+   */
   post(route, options) {
     return this.request("POST", route, options)
   }
 
+  /**
+   * Sends a PUT request to the Discord API.
+   * @param {string} route - Discord API route created with Routes method.
+   * @param {Object} options - Additional request options.
+   * @param {Object} [options.headers] - Additional headers to include in the request.
+   * @param {Object} [options.body] - The body of the request.
+   * @returns {Promise<*>} - The response from the API.
+   */
   put(route, options) {
     return this.request("PUT", route, options)
   }
 
+  /**
+   * Sends a PATCH request to the Discord API.
+   * @param {string} route - Discord API route created with Routes method.
+   * @param {Object} options - Additional request options.
+   * @param {Object} [options.headers] - Additional headers to include in the request.
+   * @param {Object} [options.body] - The body of the request.
+   * @returns {Promise<*>} - The response from the API.
+   */
   patch(route, options) {
     return this.request("PATCH", route, options)
   }
 }
 
-module.exports = { REST4Scriptable, Routes }
+globalThis.module.exports = { REST4Scriptable, Routes }
